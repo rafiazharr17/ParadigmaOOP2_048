6602d5a047e8832104462ef81806c33dd96b0f18
rafiazharr17 <rafipilo17@gmail.com>
2023-05-31 17:39:36 +0700

    membuat class pelajar menggunakan warisan atau turunan dari class manusia dan orang

--- Program MultiWaris/Program MultiWaris.cpp
+++ Program MultiWaris/Program MultiWaris.cpp
@@ -15,3 +15,34 @@ public:
 	}
 };
 
+class manusia {
+public:
+	string JenisKelamin;
+
+	manusia(string pJenisKelamin) :
+		JenisKelamin(pJenisKelamin) {
+		cout << "Manusia dibuat\n" << endl;
+	}
+	~manusia() {
+		cout << "Manusia dihapus\n" << endl;
+	}
+};
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return " Hallo, nama saya " + nama + " dengan jenis kelamin " + JenisKelamin + " dari sekolah" + sekolah + "\n\n";
+	}
+};
+
