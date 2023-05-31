71a4b59957149d14bc254e2152312ed1a19e83e9
rafiazharr17 <rafipilo17@gmail.com>
2023-05-31 17:19:03 +0700

    membuat fungsi utama program no overriding

--- Program noOverriding/Program noOverriding.cpp
+++ Program noOverriding/Program noOverriding.cpp
@@ -8,3 +8,16 @@ public:
 	}
 };
 
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
