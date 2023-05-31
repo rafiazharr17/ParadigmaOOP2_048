de4a0e52a88919fdb2b430316a2be13d2c5fb7cf
rafiazharr17 <rafipilo17@gmail.com>
2023-05-31 17:24:20 +0700

    membuat class derivedClass yang dimana ingin menggunakan turunan dari class baseClass, akan tetapi tidak bisa karena class baseClass menggunakan final

--- Program noPewarisan/Program noPewarisan.cpp
+++ Program noPewarisan/Program noPewarisan.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final {
+public:
+	virtual void perkenalan() {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
+
