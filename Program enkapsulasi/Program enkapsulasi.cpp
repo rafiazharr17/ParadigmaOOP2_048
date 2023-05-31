f1d391b1a6dd678df5f1845e61c370905da43be9
rafiazharr17 <rafipilo17@gmail.com>
2023-05-31 17:30:33 +0700

    membuat program utama enkapsulasi

--- Program enkapsulasi/Program enkapsulasi.cpp
+++ Program enkapsulasi/Program enkapsulasi.cpp
@@ -13,3 +13,18 @@ public:
 	}
 };
 
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.SetSaklarNo(0, "Lampu teras Rumah");
+	lampuRumah.SetSaklarNo(1, "Lampu ruang Tamu");
+	lampuRumah.SetSaklarNo(2, "Lampu kamar Tidur");
+	lampuRumah.SetSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.GetSaklarNo(0) << endl;
+	cout << lampuRumah.GetSaklarNo(1) << endl;
+	cout << lampuRumah.GetSaklarNo(2) << endl;
+	cout << lampuRumah.GetSaklarNo(3) << endl;
+
+	return 0;
+}
